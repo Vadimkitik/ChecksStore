93beabc8adacb8b5916403d53cda18a79c22e6fe
Vadimkitik <shakuraz777@gmail.com>
2020-04-03 14:10:36 +0300

    add material

--- ChecksStore/ClientApp/src/app/app.module.ts
+++ ChecksStore/ClientApp/src/app/app.module.ts
@@ -1,9 +1,18 @@
 ﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
+
+
 @NgModule({
-    imports: [BrowserModule, FormsModule],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        MatButtonModule
+        ],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
