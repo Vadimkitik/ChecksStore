0c1f337ec7663b5a4f43e007092662ba1d7f845c
Вадим Чижик <shakuraz777@gmail.com>
2020-04-06 23:18:48 +0300

    delai che hochesh , sdelay sort and paginator

--- ChecksStore/ClientApp/src/app/check-list/check-list.component.ts
+++ ChecksStore/ClientApp/src/app/check-list/check-list.component.ts
@@ -1,36 +1,39 @@
-﻿import { Component, OnInit, ViewChild } from '@angular/core';
+﻿import { Component, OnInit, ContentChild } from '@angular/core';
 import { DataService } from '../data.service';
 import { Check } from '../models/check';
 
 import { MatTableDataSource} from '@angular/material/table';
-import { MatSort } from '@angular/material/sort'
-import { MatPaginator } from '@angular/material/paginator'
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
     templateUrl: './check-list.component.html',
     styleUrls: ['check-list.component.css']
 })
 export class CheckListComponent implements OnInit {
-    
     displayedColumns: string[] = ['name', 'date', 'time', 'price', 'button'];
     dataSource: MatTableDataSource<Check>;
+    
+   
 
-    @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-    @ViewChild(MatSort, {static: true}) sort: MatSort;
+    @ContentChild(MatPaginator) paginator: MatPaginator;
+    @ContentChild(MatSort) sort: MatSort;
+  
 
-    constructor(private dataService: DataService) 
-    { 
-        
-    }
+    constructor(private dataService: DataService)   {  }
     
 
     ngOnInit() {
         this.load();
+    }
+
+    ngAfterViewInit(): void {
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
-    }
+      }
+
     load() {
-        this.dataService.getChecks().subscribe((data: Check[]) => this.dataSource = new MatTableDataSource(data));
+        this.dataService.getChecks().subscribe((checks: Check[]) =>{ this.dataSource = new MatTableDataSource(checks)});
     }
     delete(id: number) {
         this.dataService.deleteCheck(id).subscribe(data => this.load());
