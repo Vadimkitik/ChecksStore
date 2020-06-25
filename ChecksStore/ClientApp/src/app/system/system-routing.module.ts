364a1b812069d90e2b125a6829505b0dcc0efa63
Вадим Чижик <shakuraz777@gmail.com>
2020-06-25 14:51:44 +0300

    add components to System

--- ChecksStore/ClientApp/src/app/system/system-routing.module.ts
+++ ChecksStore/ClientApp/src/app/system/system-routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { SystemComponent } from './system.component';
+import { BillPageComponent } from './bill-page/bill-page.component';
+import { HistoryPageComponent } from './history-page/history-page.component';
+import { PlanningPageComponent } from './planning-page/planning-page.component';
+import { RecordsPageComponent } from './records-page/records-page.component';
 
 const routes: Routes = [
     {path: 'system', component: SystemComponent, children: [
-        
+        {path: 'bill', component: BillPageComponent},
+        {path: 'history', component: HistoryPageComponent},
+        {path: 'planning', component: PlanningPageComponent},
+        //{path: 'products', component: BillPageComponent},
+        {path: 'records', component: RecordsPageComponent},
+        //{path: 'users', component: BillPageComponent}
     ]}
 ];
 
--- ChecksStore/ClientApp/src/app/system/system.module.ts
+++ ChecksStore/ClientApp/src/app/system/system.module.ts
@@ -4,10 +4,20 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { SystemRoutingModule } from './system-routing.module'
 import { SystemComponent } from './system.component';
+import { BillPageComponent } from './bill-page/bill-page.component';
+import { UsersPageComponent } from './users-page/users-page.component';
+import { RecordsPageComponent } from './records-page/records-page.component';
+import { HistoryPageComponent } from './history-page/history-page.component';
+import { PlanningPageComponent } from './planning-page/planning-page.component';
 
 @NgModule({
     declarations: [
-        SystemComponent
+        SystemComponent,
+        BillPageComponent,
+        UsersPageComponent,
+        RecordsPageComponent,
+        HistoryPageComponent,
+        PlanningPageComponent
     ],
     imports: [ 
         CommonModule, 
