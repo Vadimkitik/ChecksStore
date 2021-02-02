dad8bae6e8bf0b0da37ec0f92b77a0ebf0e2ab80
Вадим Чижик <shakuraz777@gmail.com>
2021-02-03 02:09:24 +0300

    test commit

--- ChecksStore/ClientApp/src/app/auth/auth.component.ts
+++ ChecksStore/ClientApp/src/app/auth/auth.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'wfm-auth',
     templateUrl: './auth.component.html'
 })
-export class AuthComponent {}
+export class AuthComponent implements OnInit{
+    ngOnInit(): void {
+        //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+        //Add 'implements OnInit' to the class.
+        
+    }
+}
